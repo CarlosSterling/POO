21c3342b48e0f3bf37ec62063e8c6eb61f3dc867
Carlos Sterling <carlosasterling@gmail.com>
2024-08-01 18:54:15 -0500

    polimorfismo

---
+++ animalesPolimorfismo.php
@@ -0,0 +1,44 @@
+<?php
+class Animales{
+    public function hacerSonido (){
+        echo "Sonido de Animal";
+    }
+
+}
+
+class Perro extends Animales{
+    public function hacerSonido (){
+       echo "Guau Guau"."\n"; 
+
+    }
+}
+
+class Gato extends Animales{
+    public function hacerSonido (){
+        echo "Miau Miau"."\n"; 
+    }
+}
+
+class Vaca extends Animales{
+    public function hacerSonido (){
+        echo "Muuuu Muuuu"."\n"; 
+    }
+}
+
+
+//Polimormismo
+
+function hacerSonidoAnimal(Animales $animal) {
+    $animal->hacerSonido();
+}
+
+//instanciar
+
+$perro = new Perro();
+hacerSonidoAnimal($perro);
+
+$gato = new Gato();
+hacerSonidoAnimal($gato);
+
+$vaca = new Vaca();
+hacerSonidoAnimal($vaca);
